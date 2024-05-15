a88e2ee8275b986d8d29c978dfc6d16d9f60d9d5
Priyankjeet Pradhan <priyankjeetpradhan111@gmail.com>
2024-05-15 19:30:21 +0530

    home component done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Home } from './containers';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-screen h-screen flex item-start justify-center overflow-hidden'>
+      <Routes>
+        <Route path='/home/*' element={<Home />} />
+        
+        <Route path='*' element={<Navigate to={"/home"} />}/>
+      </Routes>
     </div>
   );
 }
